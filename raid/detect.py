d5345aee07a48d1ba36070c2e246f6dd25ee7d1c
liamdugan <ldugan@seas.upenn.edu>
2024-09-11 16:11:13 -0400

    run_detection no longer edits dataframe in place

--- raid/detect.py
+++ raid/detect.py
@@ -1,9 +1,12 @@
 def run_detection(f, df):
+    # Make a copy of the IDs of the original dataframe to avoid editing in place
+    scores_df = df[["id"]].copy()
+
     # Run the detector function on the dataset and put output in score column
-    df["score"] = f(df["generation"])
+    scores_df["score"] = f(df["generation"])
 
     # Convert scores and ids to dict in 'records' format for seralization
     # e.g. [{'id':'...', 'score':0}, {'id':'...', 'score':1}, ...]
-    results = df[["id", "score"]].to_dict(orient="records")
+    results = scores_df[["id", "score"]].to_dict(orient="records")
 
     return results
